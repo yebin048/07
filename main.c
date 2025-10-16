9fb73ff451c43748c725c11758bacc933b38d0a3
yebin048 <lyna0408@naver.com>
2025-10-16 15:34:34 +0900

    3번 실습

--- main.c
+++ main.c
@@ -2,18 +2,22 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int inc (int counter);
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	
-	for (i=0; i<5; i++)
-	{
-		int temp=1;
-		printf("temp=%d\n", temp);
-		temp++;
-	}
+	int i=10;
+	printf("함수 호출 전 i=%d\n", i);
+	inc(i);
+	printf("함수 호출 후 i=%d\n", i);
+
 	
 	system ("PAUSE");
-	return;
+	return 0;
+}
+
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
